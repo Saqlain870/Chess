4e5327d67f38a96f8c96a2cb8358c8d963139a5c
Bilal Quadri <bilalquadri92@gmail.com>
2013-03-18 14:48:13 -0400

    Started setting up board

--- src/cs213/chess/controls/Board.java
+++ src/cs213/chess/controls/Board.java
@@ -1,9 +1,30 @@
 package cs213.chess.controls;
 
+import cs213.chess.pieces.Piece;
+
 /**
  * @author bilalq
  *
  */
 public class Board {
 
+	private Piece[][] pieces;
+	
+	public Board() {
+		this.setPieces(null);
+	}
+
+	/**
+	 * @return the pieces
+	 */
+	public Piece[][] getPieces() {
+		return pieces;
+	}
+
+	/**
+	 * @param pieces the pieces to set
+	 */
+	public void setPieces(Piece[][] pieces) {
+		this.pieces = pieces;
+	}
 }
