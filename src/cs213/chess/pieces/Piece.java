4b84b19c5fb304d7a26c4b881dbf44bd1aa934ea
Bilal Quadri <bilalquadri92@gmail.com>
2013-03-18 14:46:29 -0400

    Started defining abstract class for piece

--- src/cs213/chess/pieces/Piece.java
+++ src/cs213/chess/pieces/Piece.java
@@ -6,4 +6,15 @@ package cs213.chess.pieces;
  */
 public abstract class Piece {
 	
+	protected char color;
+	protected char symbol;
+	protected int rank;
+	protected int file;
+	
+	protected Piece(char color, int rank, int file) {
+		this.color = color;
+		this.rank = rank;
+		this.file = file;
+	}
+	
 }
