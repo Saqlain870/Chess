3c608ff4a2a36c5e7e9bd5174c5dd15b63264429
Bilal Quadri <bilalquadri92@gmail.com>
2013-03-18 16:47:44 -0400

    Board toString completed

--- src/cs213/chess/controls/Board.java
+++ src/cs213/chess/controls/Board.java
@@ -1,19 +1,25 @@
 package cs213.chess.controls;
 
 import cs213.chess.pieces.Piece;
+import cs213.chess.utils.Helper;
 
 /**
- * @author bilalq
+ * @author Bilal Quadri
  *
  */
 public class Board {
 
 	private Piece[][] pieces;
 	
+	
+	/**
+	 * Initializes the board with pieces in their default locations.
+	 */
 	public Board() {
-		this.setPieces(null);
+		this.pieces = new Piece[8][8];
 	}
 
+	
 	/**
 	 * @return the pieces
 	 */
@@ -21,10 +27,36 @@ public class Board {
 		return pieces;
 	}
 
+	
 	/**
 	 * @param pieces the pieces to set
 	 */
 	public void setPieces(Piece[][] pieces) {
 		this.pieces = pieces;
 	}
+
+	
+	/**
+	 * Turn the board into a string that can be displayed in the command line.
+	 */
+	@Override
+	public String toString() {
+		String display = "";
+		
+		for (int i = 0; i < pieces.length; i++) {
+			for (int j = 0; j < pieces.length; j++) {
+				Piece piece = pieces[i][j];
+				if (piece == null) {
+					display += Helper.getSquareRepresentation(i, j);
+				} else {
+					display += piece.getIdentifier();
+				}
+				display += " ";
+			}
+			display += "\n";
+		}
+		
+		return display;
+	}
+	
 }
