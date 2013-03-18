9b99bf6f225eae49ce464d98b4596c4af3a797ea
Bilal Quadri <bilalquadri92@gmail.com>
2013-03-18 15:06:03 -0400

    Created Helper class

---
+++ src/cs213/chess/utils/Helper.java
@@ -0,0 +1,21 @@
+package cs213.chess.utils;
+
+/**
+ * @author bilalq
+ *
+ */
+public class Helper {
+
+	public int[] rankFileToCoords(String rankFile) {
+		return null;
+	}
+	
+	public int[] rankFileToCoords(char rank, int file) {
+		return null;
+	}
+	
+	public String coordsToRankFile(int i, int j) {
+		return null;
+	}
+	
+}
