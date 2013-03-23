4eb05546a2b1766f84e1caded2c7078e6bedd08e
Bilal Quadri <bilalquadri92@gmail.com>
2013-03-22 20:34:27 -0400

    Bishop movement

--- src/cs213/chess/pieces/Bishop.java
+++ src/cs213/chess/pieces/Bishop.java
@@ -3,6 +3,7 @@ package cs213.chess.pieces;
 import java.util.ArrayList;
 
 import cs213.chess.controls.Board;
+import cs213.chess.exceptions.IllegalFileRankException;
 
 /**
  * @author Bilal Quadri
@@ -29,8 +30,92 @@ public class Bishop extends Piece {
 	 */
 	@Override
 	public ArrayList<String> getValidMoves() {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<String> validMoves = new ArrayList<String>();
+		
+		// Flags to determine if valid moves can exist in that direction
+		boolean upUp = true;
+		boolean upDown = true;
+		boolean downUp = true;
+		boolean downDown = true;
+		
+		for (int i = 1; this.rank + i < 9; i++) {
+			char downFile = (char) (((int) this.file) - i);
+			char upFile = (char) (((int) this.file) + i);
+			int downRank = this.rank - i;
+			int upRank = this.rank + i;
+			Piece square;
+			String move;
+			
+			if (upUp) {
+				move = upFile + "" + upRank;
+				try {
+					square = this.board.getPieceAt(move);
+					if (square == null) {
+						validMoves.add(move);
+					} else {
+						if (square.color != this.color) {
+							validMoves.add(move);
+						}
+						upUp = false;
+					}
+				} catch (IllegalFileRankException e) {
+					upUp = false;
+				}
+			}
+			
+			if (upDown) {
+				move = upFile + "" + downRank;
+				try {
+					square = this.board.getPieceAt(move);
+					if (square == null) {
+						validMoves.add(move);
+					} else {
+						if (square.color != this.color) {
+							validMoves.add(move);
+						}
+						upDown = false;
+					}
+				} catch (IllegalFileRankException e) {
+					upDown = false;
+				}
+			}
+			
+			if (downUp) {
+				move = downFile + "" + upRank;
+				try {
+					square = this.board.getPieceAt(move);
+					if (square == null) {
+						validMoves.add(move);
+					} else {
+						if (square.color != this.color) {
+							validMoves.add(move);
+						}
+						downUp = false;
+					}
+				} catch (IllegalFileRankException e) {
+					downUp = false;
+				}
+			}
+			
+			if (downDown) {
+				move = downFile + "" + downRank;
+				try {
+					square = this.board.getPieceAt(move);
+					if (square == null) {
+						validMoves.add(move);
+					} else {
+						if (square.color != this.color) {
+							validMoves.add(move);
+						}
+						downDown = false;
+					}
+				} catch (IllegalFileRankException e) {
+					downDown = false;
+				}
+			}
+		}
+		
+		return validMoves;
 	}
 
 }
